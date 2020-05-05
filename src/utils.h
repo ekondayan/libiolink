dd40ddc85c26824949f2068ce5718c63f4fcc8fe
Emil Kondayan <41008491+ekondayan@users.noreply.github.com>
2020-05-05 11:19:04 +0300

    Update json library

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,23 @@
+/*
+ *   ___ ___        _     _       _
+ *  |_ _/ _ \      | |   (_)_ __ | | __
+ *   | | | | |_____| |   | | '_ \| |/ /
+ *   | | |_| |_____| |___| | | | |   <
+ *  |___\___/      |_____|_|_| |_|_|\_\
+ *
+ * Header only driver library for interfacing IO-Link devices and masters
+ * written in modern C++
+ *
+ * Version: 0.1.0
+ * URL: https://github.com/ekondayan/libiolink.git
+ *
+ * Copyright (c) 2019 Emil Kondayan
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef UTILS_H
 #define UTILS_H
 
